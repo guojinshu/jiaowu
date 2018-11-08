ed2d0cfaa1a26ed3072ec992e5dbb30e8ca53a1d
guojinshu <18860374773@163.com>
2018-11-08 13:34:50 +0800

    'b'

---
+++ public/precache-manifest.4889f0c0e0a476d21f803f50e5210c17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d34f58d73a14f715717",
+    "url": "/static/js/main.0d34f58d.chunk.js"
+  },
+  {
+    "revision": "373a305c7cbecc05dc5f",
+    "url": "/static/js/1.373a305c.chunk.js"
+  },
+  {
+    "revision": "0d34f58d73a14f715717",
+    "url": "/static/css/main.102115e2.chunk.css"
+  },
+  {
+    "revision": "609112813b67f31e9e0249651337fca4",
+    "url": "/index.html"
+  }
+];
